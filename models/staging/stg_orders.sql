5d8bfb9a8d1f13a49e7f8c879f9be11ce9e2c561
Dishant Thakur <dishant0210@outlook.com>
2024-06-25 09:46:04 +0000

    developed report_profit_by_customer and report_profit_by_product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,20 +1,21 @@
-select
--- from raw_orders
-o.orderid,
-o.orderdate,
-o.shipdate,
-o.shipmode,
-o.ordersellingprice-o.ordercostprice as orderprofit,
-o.ordersellingprice,
-o.ordercostprice,
-c.customername,
-c.segment,
-c.country,
-p.category,
-p.productname,
-p.subcategory
-from {{ ref('raw_orders') }} as o
-left join {{ ref('raw_customer') }} as c 
-on o.customerid = c.customerid
-left join {{ ref('raw_product') }} as p
-on o.productid = p.productid 
+SELECT
+  o.orderid,
+  o.orderdate,
+  o.shipdate,
+  o.shipmode,
+  o.ordersellingprice - o.ordercostprice AS orderprofit,
+  o.ordersellingprice,
+  o.ordercostprice,
+  c.customerid,
+  c.customername,
+  c.segment,
+  c.country,
+  p.category,
+  p.productid,
+  p.productname,
+  p.subcategory
+FROM {{ ref('raw_orders') }} AS o
+LEFT JOIN {{ ref('raw_customer') }} AS c 
+  ON o.customerid = c.customerid
+LEFT JOIN {{ ref('raw_product') }} AS p
+  ON o.productid = p.productid
