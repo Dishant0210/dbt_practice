a49afcab7c512b65fdc523a080720886613254cf
Dishant Thakur <dishant0210@outlook.com>
2024-06-26 06:55:09 +0000

    added delivery_team  table through csv

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -15,10 +15,13 @@ SELECT
   p.productid,
   p.productname,
   p.subcategory,
-  {{ markup ('ordersellingprice','ordercostprice') }} as markup
+  {{ markup ('ordersellingprice','ordercostprice') }} as markup,
+  d.delivery_team
 FROM {{ ref('raw_orders') }} AS o
 LEFT JOIN {{ ref('raw_customer') }} AS c 
   ON o.customerid = c.customerid
 LEFT JOIN {{ ref('raw_product') }} AS p
   ON o.productid = p.productid
+left JOIN {{ ref('delivery_team') }} as d
+ on o.shipmode = d.shipmode 
 
